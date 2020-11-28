3693368f0d1980c44690b4cbb44b5266e9ab8759
pompy <nicole.lynn.pompei@gmail.com>
2020-11-28 17:05:40 -0500

    all methods aside from overridenn compareTo

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,86 +1,35 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import com.sun.tools.internal.ws.wsdl.document.soap.SOAP12Binding;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList <SomeType> {
+public class SinglyLinkedList <SomeType> implements Comparable<SomeType> {
 
-    private static int counter;
     private Node<SomeType> head;
     private Node<SomeType> tail;
     private int length;
 
     //Default Constructor
-
-
     public SinglyLinkedList() {
         tail = null;
         head = null;
         length = 0;
     }
 
-    private class Node <SomeType>{
-
-        //reference to the next node in the chain, or null if there isn't one
-        private Node next;
-
-        //data carried by this node. could be of any type needed
-        private SomeType data;
-
-        private int index = 0;
-
-        //Node constructor
-        public Node(SomeType dataValue) {
-            this.data = dataValue;
-            this.next = null;
-        }
-
-        //another node constructor if we want to specify the node to point to
-        @SuppressWarnings("unused")
-        public Node(Node nextValue, SomeType dataValue) {
-            this.next = nextValue;
-            this.data = dataValue;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
-        public SomeType getData() {
-            return data;
-        }
-
-        @SuppressWarnings("unused")
-        public void setData(SomeType data) {
-            this.data = data;
-        }
-
-        public int getIndex() {
-            return index;
-        }
-
-        public void setIndex(int index) {
-            this.index = index;
-        }
-    }
-
-
-    public void addFirstElement(Node obj){
+    public void addFirstElement(SomeType obj){
         head = new Node<SomeType>(obj, null);
         head.setIndex(0);
     }
 
-    public void addSecondElement(Node obj){
+    public void addSecondElement(SomeType obj){
         tail = new Node<SomeType>(obj, null);
         head.setNext(tail);
         tail.setIndex(1);
     }
 
-    public void addThirdElementOnwards(Node obj){
+    public void addThirdElementOnwards(SomeType obj){
         int nextIndex = tail.getIndex() + 1;
         tail.setNext(new Node<SomeType>(obj, null));
         tail = tail.getNext();
@@ -89,7 +38,7 @@ public class SinglyLinkedList <SomeType> {
 
 
     //appends the specified element to the end of this list
-    public void add(Node data){
+    public void add(SomeType data){
         if (head == null)
             addFirstElement(data);
 
@@ -109,6 +58,15 @@ public class SinglyLinkedList <SomeType> {
         head = newHead;
         head.setIndex(0);
         shiftIndex(tail.getNext(), 0);
+    }
+
+    public void removeSecondIndex(){
+        Node<SomeType> tempNode = head.getNext();
+        Node<SomeType> nextNode = tempNode.getNext();
+        head.setNext(nextNode);
+        nextNode.setIndex(1);
+        tempNode.setNext(null);
+        shiftIndex(head.getNext(), 1);
     }
 
     public void removeInTheMiddle(Node<SomeType> tempNode, Node<SomeType> toReplace, int index){
@@ -159,9 +117,122 @@ public class SinglyLinkedList <SomeType> {
         Node<SomeType> tempNode = head;
         if(head == null)
             return false;
-        else;
+        else
+            for(int i = 0; i < this.size(); i++){
+                if(tempNode.equals(obj))
+                    return true;
+                else
+                    tempNode = tempNode.getNext();
+                }
+            return false;
+    }
 
+    public int size(){
+        return length;
+    }
+
+    public SomeType get(int index){
+        int tempIndex = head.getIndex();
+        Node<SomeType> tempNode = (Node<SomeType>) head;
+        while(tempIndex != index){
+            tempIndex++;
+            tempNode = tempNode.getNext();
+        }
+        return tempNode.getObj();
+    }
+
+    public void set(int index, SomeType obj){
+        int tempIndex = head.getIndex();
+        Node<SomeType> tempNode = head;
+        while(tempIndex != index){
+            tempIndex++;
+            tempNode = tempNode.getNext();
+        }
+        tempNode.setObj(obj);
+    }
+
+    public int find(SomeType obj){
+        Node<SomeType> tempNode = head;
+        int nodeCount = 0;
+        if(head == null)
+            return -1;
+        while(tempNode != null){
+            if(tempNode.getObj() == obj)
+                return nodeCount;
+            nodeCount++;
+            tempNode = tempNode.getNext();
+        }
+        return -1;
+    }
+
+    public SinglyLinkedList<SomeType> copyBySize(){
+        SinglyLinkedList<SomeType> copy = new SinglyLinkedList<SomeType>();
+        Node<SomeType> tempNode = head;
+        while(tempNode != null){
+            copy.add(tempNode.getObj());
+            tempNode = tempNode.getNext();
+        }
+        return copy;
+    }
+
+    public SinglyLinkedList<SomeType> copy(){
+        int sizeOfCopy = this.size();
+        if(sizeOfCopy == 0)
+            return new SinglyLinkedList<SomeType>();
+        else
+            return copyBySize();
+    }
+
+    public SinglyLinkedList<SomeType> slice(int startIndexInclusive, int endIndexInclusive){
+        SinglyLinkedList<SomeType> copy = this.copy();
+        for (int i = 0; i < copy.size(); i++) {
+            if(i < startIndexInclusive || i >= endIndexInclusive)
+                copy.remove(i);
+        }
+        return copy;
+    }
+
+    public void reverse(){
+        int size =  this.size();
+        for (int i = 0; i < size; i++) {
+            this.add(this.get(size -1 - i));
+        }
+        for (int i = 0; i < size; i++) {
+            this.remove(0);
+        }
+    }
+
+    public void swap(SomeType obj1, SomeType obj2){
+        Node<SomeType> tempNode = head;
+        int indexObj1 = -1;
+        int indexObj2 = 1;
+        while(tempNode != null){
+            if(tempNode.getObj() == obj1)
+                indexObj1 = tempNode.getIndex();
+            else if (tempNode.getObj() == obj2)
+                indexObj2 = tempNode.getIndex();
+            tempNode = tempNode.getNext();
+        }
+        this.set(indexObj2, obj1);
+        this.set(indexObj1, obj2);
+    }
+
+    public void bubbleSort(){
+        for (int i = 0; i < length - 1; i++) {
+            SomeType currentIndexI = get(i);
+            for (int j = i + 1; j < length ; j++) {
+                SomeType currentIndexJ = get(j);
+                if(currentIndexI.compareTo(currentIndexJ) > 0){
+                    swap(currentIndexI, currentIndexJ);
+                    currentIndexI = currentIndexJ;
+                }
+            }
+        }
     }
 
 
+
+    public int compareTo(SomeType o) {
+        return 0;
+    }
 }
